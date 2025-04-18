efc3c5c84d3a4f056a31391c00ec541ed30d7a72
mrxSteve <steventobar38@gmail.com>
2025-04-17 21:30:47 -0600

    fix: atributo privado CategoriaRequest

--- src/main/java/com/devsteve/product_mservice/application/dto/req/CategoriaRequest.java
+++ src/main/java/com/devsteve/product_mservice/application/dto/req/CategoriaRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 public class CategoriaRequest {
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
-    String nombre;
+    private String nombre;
 
     public String getNombre() {
         return nombre;
